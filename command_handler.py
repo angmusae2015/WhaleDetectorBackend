056f50fbdeb370827f3f8ae35fdf878ddf7719b9
angmusae2015 <angmusae2015@gmail.com>
2023-08-26 08:00:45 +0000

    Add 'addchannel' command

--- command_handler.py
+++ command_handler.py
@@ -45,5 +45,14 @@ def add_alarm(message):
     bot.send_message(chat_id=chat_id, text="알림 추가하기", reply_markup=markup)
 
 
+@bot.message_handler(commands=['addchannel'])
+def add_channel(message):
+    chat_id = message.chat.id
+
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton(text="열기", web_app=WebAppInfo(f"https://whaledetector.kro.kr/add-channel?chat_id={chat_id}")))
+
+    bot.send_message(chat_id=chat_id, text="채널 등록하기", reply_markup=markup)
+
 if __name__ == "__main__":
     bot.infinity_polling()
