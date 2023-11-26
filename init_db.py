84b5c64400e1b32d9e39085060337a8b82f71949
angmusae2015 <angmusae2015@gmail.com>
2023-11-26 10:31:53 +0000

    UPDATE
    Change every table and column name to lower case

--- init_db.py
+++ init_db.py
@@ -11,36 +11,36 @@ conn.autocommit = True
 cur = conn.cursor()
 
 # 거래소 정보 테이블
-cur.execute("""CREATE TABLE Exchange (
-    ExchangeID SERIAL PRIMARY KEY,
-    ExchangeName TEXT NOT NULL
+cur.execute("""CREATE TABLE exchange (
+    exchange_id SERIAL PRIMARY KEY,
+    exchange_name TEXT NOT NULL
 );""")
-cur.execute("""INSERT INTO Exchange (ExchangeName) VALUES ('업비트');""")
-cur.execute("""INSERT INTO Exchange (ExchangeName) VALUES ('바이낸스');""")
+cur.execute("""INSERT INTO exchange (exchange_name) VALUES ('업비트');""")
+cur.execute("""INSERT INTO exchange (exchange_name) VALUES ('바이낸스');""")
 
 # 채팅 설정 테이블
-cur.execute("""CREATE TABLE Chat (
-    ChatID BIGINT PRIMARY KEY
+cur.execute("""CREATE TABLE chat (
+    chat_id BIGINT PRIMARY KEY
 );""")
 
 # 채널 정보 테이블
-cur.execute("""CREATE TABLE Channel (
-    ChannelID BIGINT PRIMARY KEY,
-    ChannelName TEXT NOT NULL
+cur.execute("""CREATE TABLE channel (
+    channel_id BIGINT PRIMARY KEY,
+    channel_name TEXT NOT NULL
 );""")
     
 # 알림 설정 규칙 테이블
-cur.execute("""CREATE TABLE Alarm (
-    AlarmID SERIAL PRIMARY KEY,
-    ChannelID BIGINT NOT NULL,
-    ExchangeID INTEGER NOT NULL,
-    BaseSymbol TEXT NOT NULL,
-    QuoteSymbol TEXT NOT NULL,
-    AlarmCondition JSON NOT NULL,
-    IsEnabled BOOLEAN NOT NULL DEFAULT TRUE,
+cur.execute("""CREATE TABLE alarm (
+    alarm_id SERIAL PRIMARY KEY,
+    channel_id BIGINT NOT NULL,
+    exchange_id INTEGER NOT NULL,
+    base_symbol TEXT NOT NULL,
+    quote_symbol TEXT NOT NULL,
+    alarm_condition JSON NOT NULL,
+    is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
 
-    FOREIGN KEY (ExchangeID) REFERENCES Exchange(ExchangeID) ON DELETE CASCADE,
-    FOREIGN KEY (ChannelID) REFERENCES Channel(ChannelID) ON DELETE CASCADE
+    FOREIGN KEY (exchange_id) REFERENCES exchange(exchange_id) ON DELETE CASCADE,
+    FOREIGN KEY (channel_id) REFERENCES channel(channel_id) ON DELETE CASCADE
 );""")
 
 conn.commit()
